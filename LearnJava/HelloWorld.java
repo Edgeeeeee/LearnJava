dd962d5c7c511c09c3c090f3a95276bbefa037b9
Edgeeeeee <1602278333@qq,com>
2019-07-06 18:55:49 +0800

    new Java project

--- LearnJava/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld{
-	//类体
-	public static void main(String[] args) {//程序入口
-		//输出
-		System.out.println("hello world");
-
-	}
-
-}
---
+++ src/my/package1/HelloWorld.java
@@ -0,0 +1,11 @@
+package my.package1;
+
+public class HelloWorld{
+    //类体
+    public static void main(String[] args) {//程序入口
+        //输出
+        System.out.println("hello world");
+
+    }
+
+}
